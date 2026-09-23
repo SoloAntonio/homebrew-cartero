47482d327f6bb951fdb0fb65590623cab523bd31
Clément <porkepix@gmail.com>
2026-09-23 11:18:39 +0200

    fix: verified in the url stanza is now deprecated

--- Casks/cartero.rb
+++ Casks/cartero.rb
@@ -2,15 +2,13 @@ cask "cartero" do
   version "26.0"
 
   on_arm do
-            sha256 "e13b6e09ccf66578099925ede11f784dc5836f069dd6eba0560c1bc3a7eafb2f"
+    sha256 "e13b6e09ccf66578099925ede11f784dc5836f069dd6eba0560c1bc3a7eafb2f"
     url "https://github.com/danirod/cartero/releases/download/v#{version}/Cartero-#{version}-macOS-arm64.dmg",
-        verified: "github.com/danirod/cartero/"
   end
 
   on_intel do
-            sha256 "90cb35be256bf2bdf7f9ccd7a7e5856de5125f1606d24732d612e3b9f4ef242c"
+    sha256 "90cb35be256bf2bdf7f9ccd7a7e5856de5125f1606d24732d612e3b9f4ef242c"
     url "https://github.com/danirod/cartero/releases/download/v#{version}/Cartero-#{version}-macOS-x64.dmg",
-        verified: "github.com/danirod/cartero/"
   end
 
   name "Cartero"
